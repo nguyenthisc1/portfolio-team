8526ca34235f634a2ac02d9cc750e11235aa3c85
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-10-31 00:05:04 +0700

    create: Heading animation

---
+++ apps/web/src/features/animations/Heading.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+import React, { JSX, useRef } from 'react'
+gsap.registerPlugin(ScrollTrigger)
+
+type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+    text: string
+    className: string
+    as?: HeadingLevel
+}
+
+export default function Heading({ text, className, as = 1, ...props }: Props) {
+    const Tag = `h${as}` as keyof JSX.IntrinsicElements
+    const ref = useRef<HTMLDivElement>(null)
+
+    useGSAP(
+        () => {
+            if (!ref.current) return
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ref.current,
+                    start: 'top 40%',
+                    end: 'bottom 20%',
+                    scrub: true,
+                    // markers: true,
+                },
+            })
+
+            const filledElement = ref.current.querySelector('.tt-heading-filled')
+            if (filledElement) {
+                tl.to(filledElement, {
+                    clipPath: 'polygon(-5% 0%, 105% 0%, 105% 120%, -5% 120%)',
+                })
+            }
+        },
+        { scope: ref },
+    )
+    return (
+        <div ref={ref} className="tt-heading-wrapper" {...props}>
+            {React.createElement(Tag, { className: `tt-heading-stroke ${className}` }, text)}
+            <span className={`tt-heading-filled ${className}`}>{text}</span>
+        </div>
+    )
+}
