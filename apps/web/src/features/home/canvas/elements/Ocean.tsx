18b58cd502f5bd0137ce9e068e6f10ab5f5b70b3
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-11-08 21:55:04 +0700

    update: Ocean animations

--- apps/web/src/features/home/canvas/elements/Ocean.tsx
+++ apps/web/src/features/home/canvas/elements/Ocean.tsx
@@ -1,5 +1,9 @@
 /* eslint-disable react/no-unknown-property */
+import { useGlobal } from '@/shared/stores/global'
+import { useGSAP } from '@gsap/react'
 import { extend, useFrame, useLoader, useThree } from '@react-three/fiber'
+import gsap from 'gsap'
+import { useControls } from 'leva'
 import { useMemo, useRef } from 'react'
 import * as THREE from 'three'
 import { Water } from 'three-stdlib'
@@ -7,47 +11,99 @@ import { Water } from 'three-stdlib'
 extend({ Water })
 
 export default function Ocean() {
-    const ref = useRef<THREE.Mesh | null>(null)
+    const groupRef = useRef<THREE.Group | null>(null)
+    const waterRef = useRef<THREE.Mesh | null>(null)
+    const isAccess = useGlobal((state) => state.isAccess)
+
+    const leva = useControls('Ocean Group', {
+        posX: { value: 206, min: -500, max: 500, step: 1 },
+        posY: { value: -0, min: -500, max: 500, step: 1 },
+        posZ: { value: 0, min: -500, max: 500, step: 1 },
+        rotX: { value: -0.32, min: -Math.PI, max: Math.PI, step: 0.01 },
+        rotY: { value: -0.06, min: -Math.PI, max: Math.PI, step: 0.01 },
+        rotZ: { value: 0.34, min: -Math.PI, max: Math.PI, step: 0.01 },
+    })
+
     const gl = useThree((state) => state.gl)
     const waterNormals = useLoader(THREE.TextureLoader, 'images/waternormals.jpeg')
     waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
-    const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
+    const geom = useMemo(() => new THREE.PlaneGeometry(1024, 1024), [])
     const config = useMemo(
         () => ({
-            textureWidth: 512,
-            textureHeight: 512,
+            textureWidth: 1024,
+            textureHeight: 1024,
             waterNormals,
-            // sunColor: 0x000000,
             sunColor: 0xffffff,
             waterColor: 0x000000,
-            distortionScale: 7,
+            distortionScale: 3,
             fog: false,
             format: gl.capabilities.isWebGL2 ? THREE.RGBAFormat : THREE.RGBFormat,
-            toneMapping: THREE.UVMapping,
             toneMappingExposure: 10,
             clipBias: 0.9,
-            alpha: 0.5,
+            alpha: 1,
         }),
         [waterNormals, gl.capabilities.isWebGL2],
     )
 
     const water = useMemo(() => new Water(geom, config), [geom, config])
 
-    // Animate the water
     useFrame((_, delta) => {
-        if (ref.current && (ref.current as any).material?.uniforms?.time) {
-            ;(ref.current as any).material.uniforms.time.value += delta * 0.3
+        if (waterRef.current && (waterRef.current as any).material?.uniforms?.time) {
+            ;(waterRef.current as any).material.uniforms.time.value += delta * 0.3
         }
 
-        if ((ref.current as any)?.material?.uniforms?.normalSampler) {
-            const normalScale = (ref.current as any).material.uniforms.normalScale
+        if ((waterRef.current as any)?.material?.uniforms?.normalSampler) {
+            const normalScale = (waterRef.current as any).material.uniforms.normalScale
             if (normalScale && normalScale.value && waterNormals) {
                 waterNormals.offset.x += delta * -0.008
             }
         }
+
+        if (groupRef.current) {
+            groupRef.current.position.set(leva.posX, leva.posY, leva.posZ)
+            groupRef.current.rotation.set(leva.rotX, leva.rotY, leva.rotZ)
+        }
     })
 
+    useGSAP(() => {
+        if (!groupRef.current || !isAccess) return
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#profile',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: 1.5,
+                // markers: true,
+            },
+        })
+
+        tl.to(leva, {
+            rotX: 0,
+            // rotY: 0.05,
+            rotZ: 0,
+            // posY: 0.02,
+            // posX: 0,
+            ease: 'power2.inOut',
+            duration: 3,
+            onUpdate: () => {
+                if (groupRef.current) {
+                    // console.log('rotX:', leva.rotX, 'rotY:', leva.rotY, 'rotZ:', leva.rotZ)
+                    groupRef.current.position.set(leva.posX, leva.posY, leva.posZ)
+                    groupRef.current.rotation.set(leva.rotX, leva.rotY, leva.rotZ)
+                }
+            },
+        })
+    }, [isAccess])
+
     return (
-        <primitive object={water} ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} />
+        <group ref={groupRef}>
+            <primitive
+                ref={waterRef}
+                object={water}
+                position={[0, 0, 0]}
+                rotation={[-Math.PI / 2, 0, 0]}
+            />
+        </group>
     )
 }
