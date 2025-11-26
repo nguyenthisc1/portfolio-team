6e9159e686e3f9e8cfb77c20c5fdbb70d3eb2354
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-11-26 21:24:22 +0700

    update: images fetch

--- apps/dashboard/app/images/page.tsx
+++ apps/dashboard/app/images/page.tsx
@@ -1,20 +1,10 @@
 import ImagesPage from 'features/images/components/ImagesPage'
-import { getImages } from 'shared/lib/server/images'
+import { getImages } from 'server/images'
 
 export default async function Page(props: {
     searchParams: Promise<Record<string, string | string[]>>
 }) {
     const searchParams = await props.searchParams
 
-    const page = searchParams.page ? Number(searchParams.page) : 1
-    const imagesPerPage = 10
-    const offset = (page - 1) * imagesPerPage
-
-    const { images, hasMore } = await getImages({
-        limit: imagesPerPage,
-        offset,
-    })
-
-    return <ImagesPage initialImages={images} initialHasMore={hasMore} />
+    return <ImagesPage />
 }
-export const dynamic = 'force-dynamic'
