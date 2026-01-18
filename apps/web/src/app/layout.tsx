e8520b67fff3d64d28e4a4e7634fa7749f305de8
Thi Nguyễn <nguyenthisc1@gmail.com>
2026-01-18 16:26:11 +0700

    update: seo meta web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,11 +4,9 @@ import StarGalaxy from '@/features/home/canvas/scene/StarGalaxy'
 import { getHomeContent } from '@/server/homeContent'
 import Loading from '@/shared/components/Loading'
 import SoundBar from '@/shared/components/SoundBar'
-import { PROJECT_DESCRIPTION, PROJECT_NAME } from '@/shared/consts/common'
 import { GsapProvider } from '@/shared/providers/GsapProvider'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import '@workspace/ui/web.css'
-import { Metadata } from 'next'
 import localFont from 'next/font/local'
 
 const ibmPlexSansCondensed = localFont({
@@ -43,22 +41,24 @@ const oswald = localFont({
     variable: '--font-primary',
 })
 
-export const metadata: Metadata = {
-    title: {
-        default: PROJECT_NAME,
-        template: `%s | ${PROJECT_NAME}`,
-    },
-    description: PROJECT_DESCRIPTION,
-    openGraph: {
-        title: PROJECT_DESCRIPTION,
-        description: PROJECT_DESCRIPTION,
-        siteName: PROJECT_NAME,
-    },
-}
-
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     const data = await getHomeContent()
 
+    const seo = data?.seo || {
+        title: 'Creative Website Developer - Smart & Purposeful Web Design',
+        description:
+            'Portfolio of a creative designer and developer specializing in smart, business-driving websites, digital products, and unique design solutions.',
+        image: 'https://utfs.io/f/cuH9KLr6qOUNDavvj3BLWxFcPRGoIYwn59T2fgMyJQEiUS7O',
+        keywords: [
+            'website developer',
+            'creative designer',
+            'web development',
+            'digital products',
+            'portfolio',
+            'business growth',
+            'design solutions',
+        ],
+    }
     return (
         <html
             lang="en"
@@ -69,6 +69,15 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
             <head>
                 <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
                 <link rel="manifest" href="/favicon/site.webmanifest" />
+                <title>{seo.title}</title>
+                <meta name="description" content={seo.description} />
+                <meta name="keywords" content={seo.keywords?.join(', ')} />
+                <meta property="og:title" content={seo.title} />
+                <meta property="og:description" content={seo.description} />
+                <meta property="og:image" content={seo.image} />
+                <meta name="twitter:title" content={seo.title} />
+                <meta name="twitter:description" content={seo.description} />
+                <meta name="twitter:image" content={seo.image} />
             </head>
             <body className="antialiased">
                 <GsapProvider>
